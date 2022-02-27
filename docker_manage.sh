b7ccdc11e5ecd79806be559f51be119a4664d292
Evan Drake <evanmdrake@icloud.com>
2022-02-27 12:02:47 -0800

    added hotreloadable option for non-VSCODE dev

--- docker_manage.sh
+++ docker_manage.sh
@@ -8,24 +8,29 @@ case $1 in
 		yes | docker image prune
 	;;
 	"build")
-		# docker build . -t soundspace
 		docker-compose build
 	;;
 	"up" | "run")
-		# docker run -ip 8000:8000 -d soundspace
 		docker-compose up -d
 	;;
 	"setup")
 		source $(pwd)/.bashrc
 	;;
+	"dev")
+		# This allows for hot reload with the use of VScode
+		# using this method make sure that everything works with docker before commiting
+		flutter run -d chrome
+	;;
 	*)
 		echo ""
 		echo "dm build"
-		echo "	will build your image"
+		echo "	-will build your image"
 		echo "dm up"
-		echo "	will start the containor"
+		echo "	-will start the containor"
 		echo "dm kill"
-		echo "	will stop your image"
+		echo "	-will stop your image"
+		echo "dm dev"
+		echo "	-launch debugging instance of web in chrome tab with hot reload enabled"
 		echo ""
 	;;
 esac
