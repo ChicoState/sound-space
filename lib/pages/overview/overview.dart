2a6504823d31196a07677b22a3edd0b45c8dd7ee
Evan Drake <evanmdrake@icloud.com>
2022-02-27 14:39:05 -0800

    interactive img display working, self tracking stateful widget in overview ImageHandler

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -8,6 +8,7 @@ class ImageHandler extends StatefulWidget {
   _ImageHandlerState createState() => _ImageHandlerState();
 }
 
+// This widget is tracking its own state
 class _ImageHandlerState extends State<ImageHandler> {
   // private vars
   bool _isTapped = false;
@@ -22,37 +23,48 @@ class _ImageHandlerState extends State<ImageHandler> {
           _isTapped = !_isTapped;
         });
       },
-      child: Container(
-        child: Center(
-          child: Text(
-            _isTapped ? 'Tapped' : 'unTapped',
-            style: const TextStyle(fontSize: 32.0, color: Colors.white),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // This is teh button
+          SizedBox(
+            width: 100.0,
+            height: 50.0,
+            child: Container(
+              child: Center(
+                child: Text(
+                  _isTapped ? 'Tapped' : 'unTapped',
+                  style: const TextStyle(fontSize: 18.0, color: Colors.white),
+                ),
+              ),
+              width: 100.0,
+              height: 50.0,
+              decoration: BoxDecoration(
+                color: _isTapped ? Colors.lightGreen[700] : Colors.grey[600],
+              ),
+            ),
           ),
-        ),
-        width: 200.0,
-        height: 200.0,
-        decoration: BoxDecoration(
-          color: _isTapped ? Colors.lightGreen[700] : Colors.grey[600],
-        ),
+          // THis is the picture
+          SizedBox(
+            width: 300.0,
+            height: 200.0,
+            child: Container(
+                child: _isTapped
+                    ? Image.asset('still_art/test_img.jpg')
+                    : Text('Tap the button')),
+          ),
+        ],
       ),
     );
   }
 }
 
 class OverViewPage extends StatelessWidget {
-  const OverViewPage({Key? key}) : super(key: key);
+  OverViewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // return Center(
-    //     child: CustomText(
-    //   text: "OverView Page",
-    // ));
-    return Row(
-      children: [
-        ImageHandler(),
-        Text("Overview Page"),
-      ],
-    );
+    return ImageHandler();
   }
 }
