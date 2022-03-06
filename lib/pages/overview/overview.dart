eba95b23b5268d17c76b96cc009e23b84893f6e7
Evan Drake <evanmdrake@icloud.com>
2022-03-06 14:43:56 -0800

    new: firestore hooked up and working, Initialized in main. used in overview page

--- lib/pages/overview/overview.dart
+++ lib/pages/overview/overview.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:soundspace/widgets/custom_text.dart';
 
+// firebase deps
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ImageHandler extends StatefulWidget {
   const ImageHandler({Key? key}) : super(key: key);
 
@@ -12,6 +16,29 @@ class ImageHandler extends StatefulWidget {
 class _ImageHandlerState extends State<ImageHandler> {
   // private vars
   final _formKey = GlobalKey<FormState>();
+
+  CollectionReference urls = FirebaseFirestore.instance.collection(
+      'art-urls'); // instance of our firestore database that should be type safe
+
+  String url = ""; // this will act as our collected url
+
+  //url validator
+  String validateUrl(String? value) {
+    String pattern =
+        r'(^https://[a-zA-z/[.com,.edu,.gov,.org,.net]?]*)'; // NEEDS UPDATING LATER
+    RegExp regExp = new RegExp(pattern);
+    if (value == null || !regExp.hasMatch(value)) {
+      return "Please input a valid URL";
+    }
+    return value;
+  }
+
+  Future<void> addUrl(String url) {
+    return urls
+        .add({'name': 'test', 'url': url})
+        .then((value) => print("Added Art: $url"))
+        .catchError((e) => print("ADDING ART ERROR: $e"));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,25 +52,18 @@ class _ImageHandlerState extends State<ImageHandler> {
               border: OutlineInputBorder(),
               contentPadding: EdgeInsets.all(20.0),
             ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please Enter Text';
-              }
-              // This is the actual return that will use 'value'
-              return null;
+            validator: (String? value) {
+              return (url = validateUrl(value));
             },
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: ElevatedButton(
-              onPressed: () {
-                // Validate will return true if form is valid
-                if (_formKey.currentState!.validate()) {
-                  // form is invalid
-                  //Proccess data
-                }
-              },
               child: const Text('Submit'),
+              onPressed: () {
+                // UNVALIDATED, THIS DATA IS NOT CHECKED
+                addUrl(url);
+              },
             ),
           ),
         ],
