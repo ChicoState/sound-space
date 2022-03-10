b6b26333b7300cb199e4c95478627861666001e2
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2022-03-10 14:28:16 -0800

    firestore test (#40)
    * new: firestore hooked up and working, Initialized in main. used in overview page
    
    * fix: urls now stored to db, from form field
    
    * resolves #30, fix: db will manage art urls with named keys
    
    * fix: commented code for clarity
    
    * fixed: firestore_test branch merge with current version of main

---
+++ lib/pages/upload/image_handler.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:soundspace/widgets/custom_text.dart';
+
+// firebase deps
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ImageHandler extends StatefulWidget {
+  const ImageHandler({Key? key}) : super(key: key);
+
+  @override
+  _ImageHandlerState createState() => _ImageHandlerState();
+}
+
+//url validator
+bool validateUrl(String? value) {
+  String pattern =
+      r'(^https://[a-zA-z/[.com,.edu,.gov,.org,.net]?]*)'; // NEEDS UPDATING LATER
+  RegExp regExp = new RegExp(pattern);
+  if (value == null || !regExp.hasMatch(value)) {
+    // failed validation
+    return false;
+  }
+  // passed validation
+  return true;
+}
+
+// This widget is tracking its own state
+class _ImageHandlerState extends State<ImageHandler> {
+  // instance of our firestore database that should be type safe
+  CollectionReference urls = FirebaseFirestore.instance.collection('art-urls');
+
+  // contains input of controlled text field
+  final _urlController = TextEditingController();
+  final _nameController = TextEditingController();
+
+  Future<void> addUrl(String url, String name) {
+    // firebase fxn for writing new documents to a collection
+    return urls
+        // all documents must be added in json format "key : value"
+        .add({'name': name, 'url': url})
+        // .then is for any console output mostly for testing
+        .then((value) => print("Added Art( name: $name , url: $url )"))
+        // catch any possible errors
+        .catchError((e) => print("ADDING ART ERROR: $e"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: <Widget>[
+          // TextFeild for url
+          TextFormField(
+              controller: _urlController,
+              decoration: InputDecoration(
+                hintText: 'Insert url Here',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.all(20.0),
+              ),
+              validator: (val) {
+                //validate url
+                if (validateUrl(val)) {
+                  return null;
+                }
+                return 'Please enter a valid url';
+              }),
+          // TextFeild for name
+          TextFormField(
+              controller: _nameController, //  field handler
+              decoration: InputDecoration(
+                hintText: 'Insert name Here',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.all(20.0),
+              ),
+              validator: (val) {
+                //make sure name is not null
+                if (val != null) {
+                  return null;
+                }
+                return 'Please enter a name';
+              }),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                // validates that url is correct before ultimately adding anything to db
+                if (validateUrl(_urlController.text)) {
+                  // passed
+                  String _url =
+                      _urlController.text; // this will act as our collected url
+                  String _name = _nameController.text;
+                  addUrl(_url, _name);
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing data')));
+                } else {
+                  // failed
+                  // msg for user to change entered data
+                  Scaffold.of(context).showSnackBar(
+                      SnackBar(content: Text('Please enter valid data')));
+                }
+                // clears text fields no matter the case
+                _urlController.clear();
+                _nameController.clear();
+              },
+            ),
+          ),
+        ],
+      ), // Column
+    );
+  }
+}
---
+++ lib/pages/upload/music_handler.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class MusicForm extends StatefulWidget {
+  const MusicForm({Key? key}) : super(key: key);
+
+  @override
+  _MusicFormState createState() => _MusicFormState();
+}
+
+class _MusicFormState extends State<MusicForm> {
+  // private vars
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: <Widget>[
+          // TextFeild
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Music URL',
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.all(20.0),
+            ),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please Enter Plain Text';
+              }
+              // This is the actual return that will use 'value'
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate will return true if form is valid
+                if (_formKey.currentState!.validate()) {
+                  // form is invalid
+                  //Proccess data
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+        ],
+      ), // Column
+    );
+  }
+}
