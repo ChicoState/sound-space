599ab779cd12782ca6b73f443e61f2d5e6cc4c1d
CSCI-430 <ornorth@mail.csuchico.edu>
2022-04-03 13:28:32 -0700

    renamed MusicForm to MusicHandler to match sibling widgets

--- lib/pages/upload/music_handler.dart
+++ lib/pages/upload/music_handler.dart
@@ -12,46 +12,6 @@ class MusicHandler extends StatefulWidget {
 }
 
 class _MusicHandlerState extends State<MusicHandler> {
-  /*// private vars
-  final _formKey = GlobalKey<FormState>();
-
-  @override
-  Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        children: <Widget>[
-          // TextField
-          TextFormField(
-            decoration: const InputDecoration(
-              hintText: 'Music URL',
-              border: OutlineInputBorder(),
-              contentPadding: EdgeInsets.all(20.0),
-            ),
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please Enter Plain Text';
-              }
-              // This is the actual return that will use 'value'
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate will return true if form is valid
-                if (_formKey.currentState!.validate()) {
-                  // form is invalid
-                  //Proccess data
-                }
-              },
-              child: const Text('Submit'),
-            ),
-          ),
-        ],
-      ), // Column
-    );
-  }*/
   // instance of our firestore database that should be type safe
   CollectionReference urls = FirebaseFirestore.instance.collection('MUSIC');
 
