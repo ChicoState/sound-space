ef523b306973ff7647a2457c9c001404e5cee7f8
CSCI-430 <ornorth@mail.csuchico.edu>
2022-04-05 15:42:36 -0700

    login required

--- lib/pages/approval/approval.dart
+++ lib/pages/approval/approval.dart
@@ -1,18 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:soundspace/widgets/custom_text.dart';
+//user authentication
+import 'package:soundspace/pages/account/applicationState.dart';
+import 'package:provider/provider.dart';
+import 'package:soundspace/pages/account/authentication.dart';
+//routing
+import 'package:get/get.dart';
+import 'package:soundspace/constants/controllers.dart';
+import 'package:soundspace/helpers/responsiveness.dart';
 
 class ApprovalPage extends StatelessWidget {
   const ApprovalPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Consumer<ApplicationState>(
+      builder: (context, appState, _) => Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: const <Widget>[
-          CustomText(
-            text: "Approval Page",
-          )
-        ]);
+        children: <Widget>[
+          if (appState.loginState == ApplicationLoginState.loggedIn) ...[
+            const Text("Approval Page"),
+          ] else ...[
+            const Text('Please log in to approve/request approval'),
+            ElevatedButton(
+              onPressed: () {
+                menuController.changeActiveItemTo("Account");
+                if (ResponsiveWidget.isSmallScreen(context)) {
+                  Get.back();
+                }
+                navigationController.navigateTo("/account");
+              },
+              child: const Text('Log In'),
+            )
+          ],
+        ],
+      ),
+    );
   }
 }
