767a580ab1add1d64b4f26a6112c00bace624ae8
Evan Drake <evanmdrake@icloud.com>
2022-04-06 12:16:05 -0700

    merge conflict validator in upload solved

--- lib/pages/upload/valid.dart
+++ lib/pages/upload/valid.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:soundspace/widgets/custom_text.dart';
+//import 'package:flutter/material.dart';
+//import 'package:soundspace/widgets/custom_text.dart';
 
-//url validator
-bool validateUrl(String? value) {
-  String pattern =
-      r'(^https://[a-zA-z/[.com,.edu,.gov,.org,.net]?]*)'; // NEEDS UPDATING LATER
-  RegExp regExp = new RegExp(pattern);
-  if (value == null || !regExp.hasMatch(value)) {
-    // failed validation
-    return false;
-  }
-  // passed validation
-  return true;
-}
+////url validator
+//bool validateUrl(String? value) {
+//  String pattern =
+//      r'(^https://[a-zA-z/[.com,.edu,.gov,.org,.net]?]*)'; // NEEDS UPDATING LATER
+//  RegExp regExp = new RegExp(pattern);
+//  if (value == null || !regExp.hasMatch(value)) {
+//    // failed validation
+//    return false;
+//  }
+//  // passed validation
+//  return true;
+//}
