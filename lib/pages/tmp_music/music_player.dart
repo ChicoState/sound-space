f33654bcbac9c3b79a26f55c82bafd56e4987891
dhelms1 <derek.helms4@gmail.com>
2022-05-02 12:44:22 -0700

    music page updated to navigate to new visuals page

--- lib/pages/tmp_music/music_player.dart
+++ lib/pages/tmp_music/music_player.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:soundspace/pages/tmp_music/cover_display.dart';
 import 'package:soundspace/pages/tmp_music/media_display.dart';
 import 'package:soundspace/pages/music/url_handler.dart';
+import 'package:soundspace/pages/visuals/visuals.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class MusicPlayer extends StatefulWidget {
@@ -64,170 +65,96 @@ class _MusicPlayerState extends State<MusicPlayer> {
               )),
         ),
       )),
-      // Build image galley (horizontal)
-      Visibility(
-          visible: !isVisible,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 20),
-            child: ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    isVisible = true; // change page visibility
-                  });
-                },
-                // define button to navigate from art back to music list
-                style: ElevatedButton.styleFrom(
-                    primary: Colors.transparent, // hide large button
-                    shadowColor: Colors.transparent, // hide button shadow
-                    padding: EdgeInsets.zero,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20))),
-                // add gradient to button
-                child: Ink(
-                    decoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                            colors: [Colors.purpleAccent, Colors.blueAccent]),
-                        borderRadius: BorderRadius.circular(20)),
-                    // define container to hold the text button
-                    child: Container(
-                        height: 35,
-                        width: 100,
-                        alignment: Alignment.center,
-                        child: const Text(
-                          'View Music',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        )))),
-          )),
-      Visibility(
-          visible: !isVisible,
-          child: Expanded(
-            // Define art list (associated with the current song being played)
-            child: ValueListenableBuilder<String>(
-                valueListenable: curSong,
-                builder: (_, value, __) {
-                  return FutureBuilder<QuerySnapshot>(
-                      future: FirebaseFirestore.instance
-                          .collection('ART')
-                          .where('approvedFor', arrayContains: value)
-                          .get(),
-                      //convert documents into a list
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final List<DocumentSnapshot> documents =
-                              snapshot.data!.docs;
-                          //pass data to artListTile
-                          return ListView.builder(
-                              itemCount: documents.length,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder: ((context, index) => albumArt(
-                                  onTap: () {
-                                    // Enlarge photos to full view when clicked
-                                    String img = documents[index]['url'];
-                                    if (documents[index]['isVideo']) {
-                                      img =
-                                          "https://i.ytimg.com/vi/${url_key(img)}/hqdefault.jpg";
-                                    }
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            fullscreenDialog: false,
-                                            builder: (BuildContext context) {
-                                              return Scaffold(
-                                                  body: GestureDetector(
-                                                // add padding so images don't go to very edge
-                                                child: Padding(
-                                                  padding:
-                                                      const EdgeInsets.all(14),
-                                                  child: Center(
-                                                      child: Hero(
-                                                    // image animation when opening/closing
-                                                    tag: 'imageHero',
-                                                    child: ClipRRect(
-                                                        // add slight border to images
-                                                        borderRadius:
-                                                            BorderRadius
-                                                                .circular(6),
-                                                        child: Image.network(
-                                                          // show clicked image
-                                                          img,
-                                                          fit: BoxFit.fill,
-                                                        )),
-                                                  )),
-                                                ),
-                                                onTap: () {
-                                                  // return to art card
-                                                  Navigator.pop(context);
-                                                },
-                                              ));
-                                            }));
-                                  },
-                                  cover: documents[index]['url'],
-                                  isVideo: documents[index]['isVideo'])));
-                        } else {
-                          return const Text("No art yet!");
-                        }
-                      });
-                }),
-          )),
-      // Build Music Player
-      Visibility(
-        visible: isVisible,
-        child: Container(
-          // Search Bar for music list
-          margin: const EdgeInsets.fromLTRB(32, 24, 32, 24),
-          child: TextField(
-              controller: TextEditingController(),
-              decoration: InputDecoration(
-                  prefixIcon: const Icon(Icons.search),
-                  hintText: "Song Title",
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: const BorderSide(color: Colors.purple))),
-              onSubmitted: (value) {
-                //rebuild the FutureBuilder below w/ updated music query
-                setState(() {
-                  searchKey = value;
-                  musicQuery = FirebaseFirestore.instance
-                      .collection('MUSIC')
-                      .where('name', isGreaterThanOrEqualTo: searchKey)
-                      .where('name', isLessThan: searchKey + 'z')
-                      .get();
-                });
-              }),
-        ),
+      // Button to route user to visuals page
+      Padding(
+        padding: const EdgeInsets.only(top: 20),
+        child: ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      fullscreenDialog: false,
+                      builder: (context) => VisualPage(curSong: curSong)));
+              setState(() {
+                isVisible = true; // change page visibility
+              });
+            },
+            // define button to navigate from art back to music list
+            style: ElevatedButton.styleFrom(
+                primary: Colors.transparent, // hide large button
+                shadowColor: Colors.transparent, // hide button shadow
+                padding: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20))),
+            // add gradient to button
+            child: Ink(
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                        colors: [Colors.purpleAccent, Colors.blueAccent]),
+                    borderRadius: BorderRadius.circular(20)),
+                // define container to hold the text button
+                child: Container(
+                    height: 35,
+                    width: 100,
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'View Art',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    )))),
       ),
-      Visibility(
-          visible: isVisible,
-          child: Expanded(
-              // Music List
-              child: FutureBuilder<QuerySnapshot>(
-                  future: musicQuery,
-                  //convert documents into a list
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      final List<DocumentSnapshot> documents =
-                          snapshot.data!.docs;
-                      //pass data to customListTile
-                      return ListView.builder(
-                          itemCount: documents.length,
-                          itemBuilder: ((context, index) => mediaList(
-                              onTap: () {
-                                curSong.value = documents[index].id;
-                                // Update video being played to the clicked song
-                                _controller.load(
-                                    YoutubePlayerController.convertUrlToId(
-                                        documents[index]['url'])!);
-                                setState(() {
-                                  isVisible = false; // change page visibility
-                                });
-                              },
-                              title: documents[index]['name'],
-                              singer: documents[index]['user'], //email for now
-                              cover: documents[index]['url'])));
-                    } else {
-                      return const Text("Error");
-                    }
-                  })))
+      // Build Music List
+      Container(
+        // Search Bar for music list
+        margin: const EdgeInsets.fromLTRB(32, 24, 32, 24),
+        child: TextField(
+            controller: TextEditingController(),
+            decoration: InputDecoration(
+                prefixIcon: const Icon(Icons.search),
+                hintText: "Song Title",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: const BorderSide(color: Colors.purple))),
+            onSubmitted: (value) {
+              //rebuild the FutureBuilder below w/ updated music query
+              setState(() {
+                searchKey = value;
+                musicQuery = FirebaseFirestore.instance
+                    .collection('MUSIC')
+                    .where('name', isGreaterThanOrEqualTo: searchKey)
+                    .where('name', isLessThan: searchKey + 'z')
+                    .get();
+              });
+            }),
+      ),
+      Expanded(
+          // Music List
+          child: FutureBuilder<QuerySnapshot>(
+              future: musicQuery,
+              //convert documents into a list
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  //pass data to customListTile
+                  return ListView.builder(
+                      itemCount: documents.length,
+                      itemBuilder: ((context, index) => mediaList(
+                          onTap: () {
+                            curSong.value = documents[index].id;
+                            // Update video being played to the clicked song
+                            _controller.load(
+                                YoutubePlayerController.convertUrlToId(
+                                    documents[index]['url'])!);
+                            setState(() {
+                              isVisible = false; // change page visibility
+                            });
+                          },
+                          title: documents[index]['name'],
+                          singer: documents[index]['user'], //email for now
+                          cover: documents[index]['url'])));
+                } else {
+                  return const Text("Error");
+                }
+              }))
     ]);
   }
 }
