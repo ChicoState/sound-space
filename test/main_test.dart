d35702467b67d869d7a062c45db01435aaca4ea4
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2022-05-16 11:16:16 -0700

    Metrics replace (#117)
    * increase testing, coverage generated file is empty
    
    * Readme now takes people to code metrics options if needed
    
        pick fc97937 increase testing, coverage generated file is empty
        pick e4890cc Readme now takes people to code metrics options if needed
        pick 5b49d06 testing coverage at 2.5%
    
    * changed name in helper
    
    * fixed 2.5% testing
    
    * new coverage of 3.2%

--- test/main_test.dart
+++ test/main_test.dart
@@ -1,9 +1,12 @@
 import 'smoke_test.dart' as smoke;
 import 'helper_tests.dart' as fxns; //helper functions
 import 'url_validator.dart' as urls;
+import 'constant_tests.dart' as con;
+import 'package:soundspace/main.dart';
 
 void main() {
   smoke.main();
   fxns.main();
   urls.main();
+  con.main();
 }
--- test/url_validator.dart
+++ test/url_validator.dart
@@ -10,6 +10,18 @@ void main() {
       String? url = 'https://www.youtube.com/watch?v=n61ULEU7CO0';
       expect(true, validateUrl(url));
     });
+    test('youtube_url', () {
+      String? url = 'https://www.youtube.com/watch?v=7NOSDKb0HlU';
+      expect(true, validateUrl(url));
+    });
+    test('youtube_url_2', () {
+      String? url = 'https://www.youtube.com/watch?v=acsLxmnjMho';
+      expect(true, validateUrl(url));
+    });
+    test('youtube_url_3', () {
+      String? url = 'https://www.youtube.com/watch?v=lTRiuFIWV54';
+      expect(true, validateUrl(url));
+    });
     test('inc_url', () {
       String? url = 'www.youtube.com/watch?v=n61ULEU7CO0';
       expect(false, validateUrl(url));
